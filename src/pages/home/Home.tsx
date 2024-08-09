03a825aaf08e1c11f9962e713536471f7c811e5c
PU6ER <djakobstv@gmail.com>
2024-08-09 21:37:59 +0300

    feat: add home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.scss'
+
+const HomePage = () => {
+	return (
+		<div className={styles.hero}>
+			<span className={styles.text}>Добро пожаловать</span>
+		</div>
+	)
+}
+
+export default HomePage
