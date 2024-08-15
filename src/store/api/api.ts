be47bf868de6ba38a592352199fc8e7a457d1665
PU6ER <djakobstv@gmail.com>
2024-08-15 22:14:37 +0300

    feat: add api configuration

---
+++ src/store/api/api.ts
@@ -0,0 +1,79 @@
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
+import { GraphQLClient } from 'graphql-request'
+import { gql } from 'graphql-tag'
+import { GITHUB_TOKEN } from '../../constants/api'
+
+const API_URL = 'https://api.github.com/graphql'
+
+export const client = new GraphQLClient(API_URL)
+client.setHeader('Authorization', `Bearer ${GITHUB_TOKEN}`)
+
+// GraphQL запрос для поиска репозитория
+const GET_REPOS_QUERY = gql`
+	query getRepos($query: String!, $first: Int!) {
+		search(query: $query, type: REPOSITORY, first: $first) {
+			repositoryCount
+			edges {
+				node {
+					... on Repository {
+						name
+						description
+						primaryLanguage {
+							name
+						}
+						licenseInfo {
+							name
+						}
+						languages(first: 10) {
+							nodes {
+								name
+							}
+						}
+						stargazers {
+							totalCount
+						}
+						forks {
+							totalCount
+						}
+						updatedAt
+					}
+				}
+			}
+		}
+	}
+`
+
+// Ответ от сервера
+export interface IResponse {
+	search: {
+		repositoryCount: number
+		edges: { node: IRepositoryNode }[]
+	}
+}
+
+// Модель репозитория из ответа сервера
+export interface IRepositoryNode {
+	name: string
+	description: string | null
+	primaryLanguage: { name: string } | null
+	licenseInfo: { name: string } | null
+	languages: { nodes: { name: string }[] } | null
+	stargazers: { totalCount: number }
+	forks: { totalCount: number }
+	updatedAt: string
+}
+
+export const api = createApi({
+	baseQuery: graphqlRequestBaseQuery({ client }),
+	endpoints: builder => ({
+		getRepos: builder.query<IResponse, { query: string; first: number }>({
+			query: ({ query, first }) => ({
+				document: GET_REPOS_QUERY,
+				variables: { query, first },
+			}),
+		}),
+	}),
+})
+
+export const { useGetReposQuery } = api
