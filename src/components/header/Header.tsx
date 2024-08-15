a20599231b19a147a9fa5012465c77b3fcce06da
PU6ER <djakobstv@gmail.com>
2024-08-15 22:12:56 +0300

    feat: add search handling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,44 @@
 import Button from '@mui/material/Button'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import styles from './Header.module.scss'
 
 const Header = () => {
+	const [searchQuery, setSearchQuery] = useState('')
+
+	const navigate = useNavigate()
+	const { query } = useParams()
+
+	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault()
+		setSearchQuery(e.target.value)
+	}
+	useEffect(() => {
+		if (query) {
+			setSearchQuery(query)
+		}
+	}, [query])
 	return (
 		<header className={styles.header}>
-			<form className={styles.form}>
-				<input
-					type='text'
-					placeholder='Введите поисковой запрос'
-					className={styles.search}
-				/>
-				<Button variant='contained' size='large' color='primary'>
-					Искать
-				</Button>
-			</form>
+			<div className={styles.container}>
+				<form className={styles.form}>
+					<input
+						type='text'
+						placeholder='Введите поисковой запрос'
+						className={styles.search}
+						value={searchQuery}
+						onChange={e => handleSearch(e)}
+					/>
+					<Button
+						variant='contained'
+						size='large'
+						color='primary'
+						onClick={() => navigate('results/' + searchQuery)}
+					>
+						Искать
+					</Button>
+				</form>
+			</div>
 		</header>
 	)
 }
